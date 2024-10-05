78d5332ff915cfd4d80a8da0f434e4f8fbfc14b0
skyota <kyotas3007@gmail.com>
2024-10-06 00:54:22 +0900

    予約モデルの作成

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class Room(models.Model):
+    name = models.CharField(max_length=10)
+    floor = models.PositiveIntegerField()
+    
+    def __srt__(self):
+        return self.name
+    
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    title = models.CharField(max_length=50)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
